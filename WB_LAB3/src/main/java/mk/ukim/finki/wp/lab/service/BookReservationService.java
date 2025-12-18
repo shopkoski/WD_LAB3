adfc0af2204d7895d29ce4e2ee4dab58da00e0f0
shopkoski <sopkoski.goce@gmail.com>
2025-12-18 08:17:37 +0100

    updated security

--- WB_LAB3/src/main/java/mk/ukim/finki/wp/lab/service/BookReservationService.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.service;
-
-import mk.ukim.finki.wp.lab.model.BookReservation;
-
-public interface BookReservationService {
-    BookReservation placeReservation(String bookTitle, String readerName, String readerAddress, int numberOfCopies);
-}
